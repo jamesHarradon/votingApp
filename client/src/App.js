06695669a2728d2e180e4d95509152771ab07f31
James Harradon <james.harradon@gmail.com>
2022-03-16 08:12:44 +0000

    add style imports/amend routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './styles/App.css'
+import './styles/normalise.css'
+import './styles/styleReset.css'
+import Login from './components/login/Login';
 import Main from './components/main/Main';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 
+
 function App() {
+
   return (
     <Router>
       <div className="App">
