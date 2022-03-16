cfdaac72d6442a902b7584b9429dbd18a7d7fbd3
James Harradon <james.harradon@gmail.com>
2022-03-16 08:22:59 +0000

    amend error handling

--- server/app.js
+++ server/app.js
@@ -72,27 +72,32 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
-app.use('/voter', voterRouter);
-app.use('/candidate', candidateRouter);
-app.use('/election', electionRouter);
-app.use('/manifesto', manifestoRouter);
-app.use('/auth', authRouter);
+app.use('/api/voter', voterRouter);
+app.use('/api/candidate', candidateRouter);
+app.use('/api/election', electionRouter);
+app.use('/api/manifesto', manifestoRouter);
+app.use('/api/auth', authRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+app.use((err, req, res, next) => {
+  const { message, status } = err;
+  res.status(status || 500).send({Error: message});
+})
 
 const PORT = process.env.PORT || 4000;
 
