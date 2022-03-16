4c31f7096dd95ac3dcd9105e3ab5b2766d3d95cb
James Harradon <james.harradon@gmail.com>
2022-03-16 08:21:30 +0000

    add getPassword

--- server/api/models/auth-model.js
+++ server/api/models/auth-model.js
@@ -4,10 +4,21 @@ class AuthModel {
 
     async findOne(email) {
         try {
-            const adminData = await pool.query('SELECT * FROM admin WHERE email = $1', [email]);
-            if (adminData.rows?.length) return adminData.rows[0]
-            const data = await pool.query('SELECT * FROM (SELECT id, email, password, role, election_id FROM voter UNION SELECT id, email, password, role, election_id FROM candidate) AS users WHERE email = $1', [email]);
-            return data.rows?.length ? data.rows[0] : null
+            const admin = await pool.query('SELECT id, email, first_name, last_name, role FROM admin WHERE email = $1', [email]);
+            if (admin.rows?.length) return admin.rows[0]
+            const voter = await pool.query('SELECT id, email, first_name, last_name, has_voted, election_id, role FROM voter WHERE email = $1', [email]);
+            if (voter.rows?.length) return voter.rows[0]
+            const candidate = await pool.query('SELECT id, email, first_name, last_name, position, manifesto_id, election_id, role FROM candidate WHERE email = $1', [email]);
+            if (candidate.rows?.length) return candidate.rows[0]
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async getPassword(email, role) {
+        try {
+            const data = await pool.query(`SELECT password FROM ${role} WHERE email = $1`, [email]);
+            return data.rows?.length ? data.rows[0].password : null
         } catch (error) {
             throw new Error(error)
         }
