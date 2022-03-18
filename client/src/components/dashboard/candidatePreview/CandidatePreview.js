df6b5c9afc32c43d9a6f69a3aacbfcc48e9f0fa1
James Harradon <james.harradon@gmail.com>
2022-03-18 10:00:41 +0000

    amend class to className

--- client/src/components/dashboard/candidatePreview/CandidatePreview.js
+++ client/src/components/dashboard/candidatePreview/CandidatePreview.js
@@ -1,13 +1,16 @@
 import React from "react";
 
+
 export default function CandidatePreview() {
+
+    
     return (
         <div id='candidate-preview'>
             {/*this will map through candidates data and show candidate image - for now will replicate with p tag*/}
-            <p class='candidate-image'>candidate one</p>  
-            <p class='candidate-image'>candidate two</p> 
-            <p class='candidate-image'>candidate three</p>  
-            <p class='candidate-image'>candidate four</p> 
+            <p className='candidate-image'>candidate one</p>  
+            <p className='candidate-image'>candidate two</p> 
+            <p className='candidate-image'>candidate three</p>  
+            <p className='candidate-image'>candidate four</p> 
         </div>
     )
 }
