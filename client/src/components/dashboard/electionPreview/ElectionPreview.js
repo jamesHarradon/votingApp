3a411ec93e1c54494e5d9e6f1cc61836377164df
James Harradon <james.harradon@gmail.com>
2022-03-18 10:01:24 +0000

    amend to use state

--- client/src/components/dashboard/electionPreview/ElectionPreview.js
+++ client/src/components/dashboard/electionPreview/ElectionPreview.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useGetElectionQuery } from '../../../services/election';
+import { selectUser } from "../../../userSlice";
+import { DateTime } from 'luxon';
 
 export default function ElectionPreview() {
+
+    const electionId = useSelector(selectUser).election_id;
+    const { data } = useGetElectionQuery(electionId)
+    const dateFormated = data && DateTime.fromISO(data.date_of_election).setLocale('en-gb').toLocaleString();
+    console.log(dateFormated);
+    
     return (
         <div id='election-preview'>
-            <h2>Election: Election Name</h2>    
-            <h2>Date: 25/05/2022</h2>
+            <h2>Election: {data && data.name}</h2>    
+            <h2>Date of Election: {dateFormated}</h2>
         </div>
     )
 }
