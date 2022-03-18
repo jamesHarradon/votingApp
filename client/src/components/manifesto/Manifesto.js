dd2716238480c8b4f1b928eafd4cd2830d82a5e1
James Harradon <james.harradon@gmail.com>
2022-03-18 15:18:58 +0000

    amend render add state

--- client/src/components/manifesto/Manifesto.js
+++ client/src/components/manifesto/Manifesto.js
@@ -1,40 +1,44 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useGetCandidateByIdQuery } from "../../services/candidate";
+import { useGetManifestoByCandidateQuery } from "../../services/manifesto";
+import { selectUser } from "../../userSlice";
 
 export default function Manifesto() {
 
-    const isAdmin = true; //this is to simulate if user is admin
-    const isCandidate = true;
+    const user = useSelector(selectUser);
+    const isVoter = user.role === 'voter';
 
     const params = useParams();
 
-    useEffect(() => {
-        //something like dispatch(getManifesto(parseInt(params.id)) to show manifesto for correct candidate
-    },[])
+    const { data: candidate, isLoading: candidateLoading } = useGetCandidateByIdQuery({candidateId: params.candidateId, electionId: params.electionId})
+    const { data: manifesto, isLoading: manifestoLoading } = useGetManifestoByCandidateQuery(params.candidateId);
 
-    const candidate = {first_name: 'James', last_name: 'Harradon', position: 'President', name: 'Position of Union President at Twitter', who: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat earum beatae repellendus. Voluptatum repellat ullam officia maxime saepe exercitationem incidunt, doloremque cupiditate, officiis ab ad repellendus neque dignissimos. Eaque, eius!', what: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odit nulla, non, adipisci nisi error ea dolores odio obcaecati sequi voluptate veritatis sed! Voluptas facilis error culpa voluptatum minus eligendi.', why: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus sed fugit velit. Ab sapiente quidem assumenda expedita illum praesentium laborum aliquid, voluptate eius explicabo veritatis aperiam, quo, repellendus maiores. Corrupti!'};
     return (
         <div id= 'manifesto'>
             <div id='manifesto-head'>
-                <h2>{`${candidate.first_name} ${candidate.last_name} `}</h2>
-                <h2>{candidate.position}</h2>
-                <h2>{candidate.name}</h2>
+                {candidateLoading && <h2>Loading...</h2>}
+                <h2>{`${candidate && candidate.first_name} ${candidate && candidate.last_name} `}</h2>
+                <h2>{candidate && candidate.position}</h2>
+                <h2>{candidate && candidate.name}</h2>
             </div>
             <div id='manifesto-body'>
+                {manifestoLoading && <h2>Loading...</h2>}
                 <div id='manifesto-who'>
                     <h2>Who Am I?</h2>
-                    <p>{candidate.who}</p>
-                    {(isAdmin || isCandidate) && <button>Edit</button>}
+                    <p>{manifesto && manifesto.who}</p>
+                    {!isVoter&& <button>Edit</button>}
                 </div>
                 <div id='manifesto-what'>
                     <h2>What Do I Want To Achieve?</h2>
-                    <p>{candidate.what}</p>
-                    {(isAdmin || isCandidate) && <button>Edit</button>}
+                    <p>{manifesto && manifesto.what}</p>
+                    {!isVoter && <button>Edit</button>}
                 </div>
                 <div id='manifesto-why'>
                     <h2>Why Vote For Me?</h2>
-                    <p>{candidate.why}</p>
-                    {(isAdmin || isCandidate) && <button>Edit</button>}
+                    <p>{manifesto && manifesto.why}</p>
+                    {!isVoter && <button>Edit</button>}
                 </div>
             </div>
         </div>
