e594bd309c8ebdf7b9724e1dcb69ad1a80cc7f0d
James Harradon <james.harradon@gmail.com>
2022-03-18 10:02:05 +0000

    amend to use state

--- client/src/components/dashboard/summaries/Summaries.js
+++ client/src/components/dashboard/summaries/Summaries.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { useGetElectionQuery } from '../../../services/election';
+import { selectUser } from "../../../userSlice";
 
 export default function Summaries() {
+
+    const electionId = useSelector(selectUser).election_id;
+    console.log(electionId)
+    const { data } = useGetElectionQuery(electionId)
 
     //summaries below will contain dynamic data from state
 
     return (
         <div id='summaries'>
             <div className='summary'>
-                <h4>No. of candidates</h4>
-                <p>4</p>
+                <h4>No. of Candidates</h4>
+                <p>{data && data.number_of_candidates}</p>
             </div>
             <div className='summary'>
-                <h4>No. of voters</h4>
-                <p>30</p>
+                <h4>No. of Voters</h4>
+                <p>{data && data.number_of_voters}</p>
             </div>
             <div className='summary'>
                 <h4>Votes Cast</h4>
-                <p>25</p>
+                <p>to do</p>
             </div>
             <div className='summary'>
                 <h4>Voters yet to vote</h4>
-                <p>5</p>
+                <p>to do</p>
             </div>
         </div>
     )
