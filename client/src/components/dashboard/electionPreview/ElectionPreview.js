61a73b66fc74d3013e677deb7e51f39597538aa2
James Harradon <james.harradon@gmail.com>
2022-03-18 15:24:59 +0000

    begin to amend for admin

--- client/src/components/dashboard/electionPreview/ElectionPreview.js
+++ client/src/components/dashboard/electionPreview/ElectionPreview.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { useGetElectionQuery } from '../../../services/election';
+import { useGetElectionQuery, useGetElectionsQuery } from '../../../services/election';
 import { selectUser } from "../../../userSlice";
 import { DateTime } from 'luxon';
 
 export default function ElectionPreview() {
 
+    const [ election, setElection ] = useState(null);
+
+    const user = useSelector(selectUser);
+    const isAdmin = user.role === 'admin';
+
     const electionId = useSelector(selectUser).election_id;
     const { data } = useGetElectionQuery(electionId)
     const dateFormated = data && DateTime.fromISO(data.date_of_election).setLocale('en-gb').toLocaleString();
+
+    //const { data: adminElections } = useGetElectionsQuery()
+
     
     return (
         <div id='election-preview'>
-            <h2>Election: {data && data.name}</h2>    
+            {/* {isAdmin ?  
+                <select id='election-select' name='election' onChange={(e) => {setElection(e.target.value)}}>
+                {adminElections && adminElections.map(election => (
+                    <option id={election.id} value={election.name}>{election.name}</option>
+                ))}     
+                </select> 
+                :<h2>Election: {data && data.name}</h2> 
+            } */}
+            <h2>Election:{data && data.name}</h2>    
             <h2>Date of Election: {dateFormated}</h2>
         </div>
     )
