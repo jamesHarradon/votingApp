769b8a3d08850994067a180bd19e3a844e2ac556
James Harradon <james.harradon@gmail.com>
2022-03-19 10:50:41 +0000

    add local state and props

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Summaries from "./summaries/Summaries";
 import CandidatePreview from "./candidatePreview/CandidatePreview";
 import ElectionPreview from "./electionPreview/ElectionPreview";
+import AdminDropDown from "./adminDropDown/adminDropDown";
 
 export default function Dashboard() {
+
+    const [ electionId, setElectionId ] = useState(null);
+
     return (
         <div id='dashboard'>
-            <ElectionPreview />
-            <Summaries />
-            <CandidatePreview />
+            <ElectionPreview electionId={electionId} setElectionId={setElectionId} />
+            <Summaries electionId={electionId} />
+            <CandidatePreview electionId={electionId} />
         </div>
     )
 }
