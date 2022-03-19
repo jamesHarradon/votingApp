d70110b48bdf2aabb2ac8d452100dd1c89b48b40
James Harradon <james.harradon@gmail.com>
2022-03-19 10:56:45 +0000

    add className to link

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -12,7 +12,7 @@ export default function Sidebar() {
         <div id='sidebar'>
             <Link to='/' className='sidebar-link'>Dashboard</Link>
             <Link to={user.role === 'voter' ? '/ballot-card' : '/voters'} className='sidebar-link'>{user.role === 'voter' ? 'Ballot Card' : 'Voters'}</Link>
-            <Link to={user.role === 'candidate' ? '/manifesto' : '/candidates'} className='sidebar-link'>{user.role === 'candidate' ? 'Manifesto' : 'Candidates'}</Link>
+            <Link to={user.role === 'candidate' ? `/manifesto/${user.id}/${user.election_id}` : '/candidates'} className='sidebar-link'>{user.role === 'candidate' ? 'Manifesto' : 'Candidates'}</Link>
             <Link to='/election' className='sidebar-link'>Election</Link>
             <Link to='/results' className='sidebar-link'>Results</Link>
         </div>
