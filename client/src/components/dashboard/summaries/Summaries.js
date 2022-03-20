a71fb31fd0cb2f4c27153e2818176c2c3337d9a5
James Harradon <james.harradon@gmail.com>
2022-03-20 10:44:11 +0000

    add votes cast and yet to vote

--- client/src/components/dashboard/summaries/Summaries.js
+++ client/src/components/dashboard/summaries/Summaries.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { useSelector } from "react-redux";
 import { useGetElectionQuery } from '../../../services/election';
+import { useGetVotersByElectionQuery } from '../../../services/voter';
 import { selectUser } from "../../../userSlice";
 
 export default function Summaries({ electionId }) {
 
     const user = useSelector(selectUser);
     const id = electionId || user.election_id
-    const { data } = useGetElectionQuery(id)
+    const { data: election } = useGetElectionQuery(id);
+    const { data: voters } = useGetVotersByElectionQuery(id);
+
+    const voted = voters && voters.filter(voter => voter.has_voted).length;
+    const yetToVote = election && election.number_of_voters - voted;
     
     return (
         <div id='summaries'>
             <div className='summary'>
                 <h4>No. of Candidates</h4>
-                <p>{data && data.number_of_candidates}</p>
+                <p>{election && election.number_of_candidates}</p>
             </div>
             <div className='summary'>
                 <h4>No. of Voters</h4>
-                <p>{data && data.number_of_voters}</p>
+                <p>{election && election.number_of_voters}</p>
             </div>
             <div className='summary'>
                 <h4>Votes Cast</h4>
-                <p>to do</p>
+                <p>{voted}</p>
             </div>
             <div className='summary'>
                 <h4>Voters yet to vote</h4>
-                <p>to do</p>
+                <p>{yetToVote}</p>
             </div>
         </div>
     )
