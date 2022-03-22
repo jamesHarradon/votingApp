17d737cee780fbf1309354ed8d0ddb878bde8c38
James Harradon <james.harradon@gmail.com>
2022-03-22 20:22:03 +0000

    add changePassword endpoint

--- client/src/userSlice.js
+++ client/src/userSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+// response.text() can be used to return text if data in response is not json
+
 export const loginUser = createAsyncThunk(
     'user/loginUser', async (body, { rejectWithValue }) => {
         try {
@@ -17,7 +19,7 @@ export const loginUser = createAsyncThunk(
                 return user;
             } else {
                 rejectWithValue(null);
-                const errorMsg = await response.json()
+                const errorMsg = await response.text()
                 throw new Error(errorMsg);
             }       
         } catch (err) {
@@ -37,7 +39,7 @@ export const logoutUser= createAsyncThunk(
             if (response.ok) {
                 return null
             } else {
-                const errorMsg = await response.json()
+                const errorMsg = await response.text()
                 throw new Error(errorMsg);
             }
         } catch (err) {
@@ -62,7 +64,7 @@ export const amendUser = createAsyncThunk(
                 const user = await response.json();
                 return user;
             } else {
-                const errorMsg = await response.json()
+                const errorMsg = await response.text()
                 throw new Error(errorMsg);
             }       
         } catch (err) {
@@ -84,8 +86,32 @@ export const getUserById = createAsyncThunk(
                 return user;
             } else {
                 const errorMsg = await response.text()
-                //throw new Error(errorMsg);
-                console.log(errorMsg);
+                throw new Error(errorMsg);
+            }       
+        } catch (err) {
+            console.log(err);
+        } 
+    }
+)
+
+export const changePassword = createAsyncThunk(
+    'user/changePassword', async (data, { rejectWithValue }) => {
+        try {
+            const response = await fetch(`http://localhost:4000/api/user/amend/password/${data.id}/${data.role}`, {
+                method: 'PUT',
+                credentials: 'include',
+                mode: 'cors',
+                body: JSON.stringify(data.body),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            });
+            if (response.ok) {
+                return true;
+            } else {
+                return rejectWithValue(null);
+                // const errorMsg = await response.text()
+                // throw new Error(errorMsg);
             }       
         } catch (err) {
             console.log(err);
@@ -106,6 +132,7 @@ const userSlice = createSlice({
         // see store for logout action
         logout: state => {}
     },
+    //need to find out better way to arrange extraReducers
     extraReducers: {
         [loginUser.pending]: (state) => {
             state.isLoading = true;
@@ -156,6 +183,19 @@ const userSlice = createSlice({
             state.hasFailed = false;
         },
         [getUserById.rejected]: (state) => {
+            state.isLoading = false;
+            state.hasFailed = true;
+        },
+        [changePassword.pending]: (state) => {
+            state.isLoading = true;
+            state.hasFailed = false;
+        },
+        //does not alter state so have removed state.user - not sure if this is correct way
+        [changePassword.fulfilled]: (state) => {
+            state.isLoading = false;
+            state.hasFailed = false;
+        },
+        [changePassword.rejected]: (state) => {
             state.isLoading = false;
             state.hasFailed = true;
         }
