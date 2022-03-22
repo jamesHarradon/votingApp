789888229f62d07b57b60d829c8cd5e778f83f1b
James Harradon <james.harradon@gmail.com>
2022-03-22 08:33:22 +0000

    add dynamic class to show selected sidebar link

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -8,13 +8,18 @@ export default function Sidebar() {
 
     const user = useSelector(selectUser);
 
+    const addSelected = (id) => {   
+        const sidebarLinks = Array.from(document.getElementsByClassName('sidebar-link'));
+        sidebarLinks.map(link => link.id === id ? link.className = 'sidebar-link selected' : link.className = 'sidebar-link')
+    }
+
     return (
         <div id='sidebar'>
-            <Link to='/' className='sidebar-link'>Dashboard</Link>
-            <Link to={user.role === 'voter' ? '/ballot-card' : '/voters'} className='sidebar-link'>{user.role === 'voter' ? 'Ballot Card' : 'Voters'}</Link>
-            <Link to={user.role === 'candidate' ? `/manifesto/${user.id}/${user.election_id}` : '/candidates'} className='sidebar-link'>{user.role === 'candidate' ? 'Manifesto' : 'Candidates'}</Link>
-            <Link to='/election' className='sidebar-link'>Election</Link>
-            <Link to='/results' className='sidebar-link'>Results</Link>
+            <Link id='1' onClick={() => addSelected('1')} to='/' className='sidebar-link'>Dashboard</Link>
+            <Link id='2' onClick={() => addSelected('2')} to={user.role === 'voter' ? '/ballot-card' : '/voters'} className='sidebar-link'>{user.role === 'voter' ? 'Ballot Card' : 'Voters'}</Link>
+            <Link id='3' onClick={() => addSelected('3')} to={user.role === 'candidate' ? `/manifesto/${user.id}/${user.election_id}` : '/candidates'} className='sidebar-link'>{user.role === 'candidate' ? 'Manifesto' : 'Candidates'}</Link>
+            <Link id='4' onClick={() => addSelected('4')} to='/election' className='sidebar-link'>Election</Link>
+            <Link id='5' onClick={() => addSelected('5')} to='/results' className='sidebar-link'>Results</Link>
         </div>
     )
 }
