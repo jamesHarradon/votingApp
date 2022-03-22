3ca285f67a881c5b2d63a1d6957c6a3b140c9d8d
James Harradon <james.harradon@gmail.com>
2022-03-22 20:10:07 +0000

    add key/remove unecessary imports

--- client/src/components/dashboard/adminDropDown/adminDropDown.js
+++ client/src/components/dashboard/adminDropDown/adminDropDown.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { useGetElectionsQuery } from "../../../services/election";
 import { selectUser } from "../../../userSlice";
@@ -6,12 +6,12 @@ import { selectUser } from "../../../userSlice";
 export default function AdminDropDown({ setElectionId }) {
 
     const admin = useSelector(selectUser);
-
     const { data: elections } = useGetElectionsQuery(admin.id);
+
     return (
         <select id='election-select' name='election' onChange={(e) => {setElectionId(e.target.value)}}>
             {elections && elections.map(election => (
-                <option id={election.id} value={election.id}>{election.name}</option>
+                <option key={election.id} id={election.id} value={election.id}>{election.name}</option>
             ))}     
         </select> 
     )
