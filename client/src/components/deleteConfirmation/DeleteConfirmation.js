316b7c923adcd65bce71d275ad179930c816d410
James Harradon <james.harradon@gmail.com>
2022-04-01 16:08:17 +0100

    add electionId

--- client/src/components/deleteConfirmation/DeleteConfirmation.js
+++ client/src/components/deleteConfirmation/DeleteConfirmation.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function DeleteConfirmation({ cancelHandler, proceedHandler, name, deleteId }) {
+export default function DeleteConfirmation({ cancelHandler, proceedHandler, name, deleteId, deleteElectionId }) {
 
 
     return (
         <div className='modal-container'>
             <div className='modal'>
                 <h2>Are you sure you wish to delete {name}?</h2>
-                <button onClick={() => proceedHandler(deleteId)}>Yes</button>
+                <button onClick={() => proceedHandler(deleteId, deleteElectionId)}>Yes</button>
                 <button onClick={() => cancelHandler()}>No</button>
             </div>
         </div>
