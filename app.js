b230668bea3715e1626f05585495c0d6b60f806e
James Harradon <james.harradon@gmail.com>
2022-04-01 11:27:44 +0100

    add catch all route

--- app.js
+++ app.js
@@ -79,6 +79,10 @@ app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/result', resultRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
+
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
