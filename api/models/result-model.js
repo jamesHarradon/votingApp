ec2bd00526a3fdaa0a479f0af4d22727361d23bd
James Harradon <james.harradon@gmail.com>
2022-04-02 12:47:38 +0100

    remove position from returned data

--- api/models/result-model.js
+++ api/models/result-model.js
@@ -21,7 +21,7 @@ class ResultModel {
 
     async getVotedCandidateByVoter(id, electionId) {
         try {
-            const data = await pool.query('SELECT candidate.id, email, first_name, last_name, position, manifesto_id, election_id, name, who, what, why FROM candidate JOIN election ON election_id = election.id JOIN manifesto ON manifesto_id = manifesto.id WHERE candidate.id = (SELECT candidate_id FROM voters_candidates WHERE voter_id = $1 AND election_id = $2)', [id, electionId]);
+            const data = await pool.query('SELECT candidate.id, email, first_name, last_name, manifesto_id, election_id, name, who, what, why FROM candidate JOIN election ON election_id = election.id JOIN manifesto ON manifesto_id = manifesto.id WHERE candidate.id = (SELECT candidate_id FROM voters_candidates WHERE voter_id = $1 AND election_id = $2)', [id, electionId]);
             return data?.rows.length ? data.rows[0] : null; 
         } catch (error) {
             throw new Error(error)
@@ -30,7 +30,7 @@ class ResultModel {
 
     async getResults(electionId) {
         try {
-            const data = await pool.query('SELECT candidate_id, first_name, last_name, email, position, role, name, COUNT(candidate_id) AS votes FROM voters_candidates vc JOIN candidate c ON vc.candidate_id = c.id JOIN election e ON e.id = c.election_id WHERE vc. election_id = $1 GROUP BY candidate_id, first_name, last_name, email, position, role, name ORDER BY 8 DESC', [electionId]);
+            const data = await pool.query('SELECT candidate_id, first_name, last_name, email, role, name, COUNT(candidate_id) AS votes FROM voters_candidates vc JOIN candidate c ON vc.candidate_id = c.id JOIN election e ON e.id = c.election_id WHERE vc. election_id = $1 GROUP BY candidate_id, first_name, last_name, email, position, role, name ORDER BY 8 DESC', [electionId]);
             return data.rows?.length ? data : null
         } catch (error) {
             throw new Error(error)
