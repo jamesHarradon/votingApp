e11bb812fe3f828dd89aa26a3399112b9720f4c8
James Harradon <james.harradon@gmail.com>
2022-04-02 17:47:46 +0100

    add about info and github logo

--- client/src/components/topbar/Topbar.js
+++ client/src/components/topbar/Topbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { logoutUser } from '../../userSlice'
+import ghLogo from '../../github.png'
 
 export default function Topbar() {
 
@@ -15,6 +16,13 @@ export default function Topbar() {
     return (
         <div id='topbar'>
             <h1 id='heading'>Voter</h1>
+            <div id='app-about'>
+                <div>
+                    <p>app built by James Harradon</p>
+                    <p>with PostgreSQL, Express, React and Node</p>
+                </div>
+                <a href='https://github.com/jamesHarradon/votingApp'><img src={ghLogo} alt='github logo'></img></a>
+            </div>
             <div className='topbar-links'>
                 <Link className='link' to='/profile'>Profile</Link>
                 <button onClick={logoutHandler}>Logout</button>
