e2c5b6390a840f05590ef48c74b25378a824503d
James Harradon <james.harradon@gmail.com>
2022-04-09 16:47:26 +0100

    add edit-cookie route

--- api/routes/auth-route.js
+++ api/routes/auth-route.js
@@ -39,6 +39,40 @@ authRouter.post('/login', (req, res, next) => {
     })(req, res, next);
 })
 
+//edits cookie when admin adds/removes election. This is necessary for authorisation to see newly created elections.
+authRouter.put('/edit-cookie/:action/:electionId', passport.authenticate('jwt-admin', { session: false }), (req, res, next) => {
+
+    if(req.params.action === 'add') {
+        req.jwtPayload.election_ids.push(parseInt(req.params.electionId))
+    }
+
+    if(req.params.action === 'remove') {
+        req.jwtPayload.election_ids = req.jwtPayload.election_ids.filter(id => id !== parseInt(req.params.electionId))
+    }
+
+    const payload = {
+        id: req.user.id,
+        email: req.user.email,
+        role: req.user.role,
+        election_ids: req.jwtPayload.election_ids
+    }
+
+    const options = {
+        subject: `${req.user.id}`,
+        expiresIn: 3600
+    }
+    const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+
+    res.cookie('jwt-votingApp', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+        sameSite: isProduction ? 'none' : 'lax',
+        secure: isProduction ? true : false,
+    })
+    
+    res.json({...payload, first_name: req.user.first_name, last_name: req.user.last_name});
+})
+
 authRouter.post('/logout', (req, res, next) => {
     res.clearCookie('jwt-votingApp');
     return res.status(200).send();
