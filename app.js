e1d9df50fe609e335b6069195e70c366990f588e
James Harradon <james.harradon@gmail.com>
2022-04-21 19:12:08 +0100

    temp fix for bug

--- app.js
+++ app.js
@@ -106,6 +106,11 @@ app.use((err, req, res, next) => {
   res.status(status || 500).send({Error: message});
 })
 
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
 
 
 module.exports = app;
